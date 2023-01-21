8186b42a599888f3fa12ea8588fd9b2a522b517b
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-21 12:52:24 +0000

    Creating function for hour slots in JS file.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,6 +3,28 @@ var currentDay = moment().format("dddd, MMMM Do");
     // Targets ID #currentDay with current day, month date. 
     $("#currentDay").text(currentDay);
 
-// $( function(){
-    $("#timeSlots").selectable
-// });
+// Set variable for save button .saveBTN and textarea .hourSlot
+var saveTextBtn = $(".saveBtn");
+var textInSlot = $(".hourSlot");
+
+
+// Create event listener function using Jquery to store textarea value.
+saveTextBtn.on("click", function() {
+//  alert("Save Success");
+console.log(textInSlot.val());
+textInSlot.text(textInSlot.val());
+
+// POSSIBLE LOOP THROUGH OF TEXT AREA VALUES 
+// TO SEE WHAT NEEDS SAVING WHEN SaveTextBtn is clicked!!!
+var textInput = $('textarea:input');
+var textValue =[];
+
+$.each(textInput, function() {
+    textValue.push($(this).val());
+});
+
+// keep clear if no input
+$('textarea[type="text"]').val('', false);
+
+});
+
