d2b284d26ca4920fdb6bf84389b585cedccd82a2
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-24 13:44:29 +0000

    Adde Pseudo-code to script.js File.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,5 @@
+// FOR TESTING PURPOSE ONLY --------------------------------
+
 // Set current day/date to page using moment.js
 var currentDay = moment().format("dddd, MMMM Do");
     // Targets ID #currentDay with current day, month date. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,12 @@ var textInSlot = $(".hourSlot");
 
 // Create event listener function using Jquery to store textarea value.
 saveTextBtn.on("click", function() {
-    
+
+// Color hourSlot text according to past, present, or future.
+
+// Append time and color to divs?
+
+// LocalStaorage to setItem and getItem
+
+
 });
