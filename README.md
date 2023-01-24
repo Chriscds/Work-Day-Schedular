4eb73da7ca8851fb6ec51bb8c8f7b6dac11353bd
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-24 19:39:26 +0000

    updated README

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 Calendar application for a Work Day Schedule.
 
 ## Description
+(Work in progress. Currently deciding which is the best way for the textareas to be added to the html page.) 
 
+This application when it is fully working, will show a day scheduler with hour slots. User can enter a text input within the hour slot that when saved, will store in local storage.
+The time slots will show different colours depending on whether the hour time slot is in the past, present, or future.
 
 ## Built With 
 * HTML
@@ -11,9 +14,10 @@ Calendar application for a Work Day Schedule.
 * JQuery
 
 ## Screenshot
-![Work Day Scheduler]
+![Work Day Scheduler](assets/images/Work-Day-Scheduler-screenshot.png)
 
 ## Usage
+You can view the work day scheduler [here]()
 
 ## Licence
 Please refer to the LICENCE in the repository.
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,34 @@ var currentDay = moment().format("dddd, MMMM Do");
 var saveTextBtn = $(".saveBtn");
 var textInSlot = $(".hourSlot");
 
+// Color hourSlot text according to past, present, or future.
+var currentHour = moment().format("H");
+
+// Append time and color to divs? 
+for (var i = 9; i < 17; i++) { 
+    var hour;
+    if (currentHour == i) {
+        hour = 'present';
+    } else if (currentHour > i) {
+        hour = 'future';
+    } else if (currentHour < i) {
+        hour = 'past';
+    }
+
+    // Add color class to textarea
+    var timeColors = '<textarea class= hourSlot ${currentHour}></textarea>'; 
+    $(".time-block").append(timeColors);
+};
+
+
 
 // Create event listener function using Jquery to store textarea value.
 saveTextBtn.on("click", function() {
 
-// Color hourSlot text according to past, present, or future.
 
-// Append time and color to divs?
 
-// LocalStaorage to setItem and getItem
+
+// LocalStorage to setItem and getItem
 
 
 });
